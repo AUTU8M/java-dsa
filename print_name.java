41fafe03a0b347fd1d4c63aa1432592fd7397490
Shubham Sarkar <93968079+AUTU8M@users.noreply.github.com>
2025-06-01 23:12:35 +0530

    Add files via upload
    lmao ded

---
+++ print_name.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class print_name {
+   public static void main(String[] args) {
+    Scanner input =  new Scanner(System.in);
+    String name = input.nextLine();
+    
+    System.out.println("hello "+ name +" how you doing?");
+
+
+   }
+}
