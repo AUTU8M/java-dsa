f4a75b0ad097b88c84194412ddd4728d45f923db
Shubham Sarkar <93968079+AUTU8M@users.noreply.github.com>
2025-06-04 22:42:48 +0530

    Add files via upload
    alr8 now gon staart the function qs

---
+++ Questions_solve/Armstrong.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter the first number: ");
+        int start = input.nextInt();
+        System.out.print("enter the second number: ");
+        int end = input.nextInt();
+
+        System.out.println("the armstrong number between " + start + " and " + end + " is: ");
+        for (int num = start; num < end; num++) {
+            int original = num;
+            int temp = num;
+            int sum = 0;
+
+            int numlen = String.valueOf(num).length(); //convert to string then to length
+
+            while (temp > 0) {
+                int digit = temp % 10;
+                sum += Math.pow(digit, numlen);
+                temp = temp / 10;
+            }
+            if(sum==original){
+                System.out.println(original);
+            }
+        }
+        input.close();
+
+    }
+}
---
+++ Questions_solve/Main.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public  class  Main{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter a number: ");
+        float cel = input.nextFloat();
+
+        float fertemp = (cel * 9/5)+32;
+
+        System.out.println("the temp is fernheit is: " + fertemp);
+
+    }
+}
