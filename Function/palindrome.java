efbc4977050252db7f065e811199615907460159
Shubham Sarkar <shubhamsarkar2712@gmail.com>
2025-07-14 18:55:08 +0530

    function part done

--- Function/palindrome.java
+++ Function/palindrome.java
@@ -1,2 +1,26 @@
-public class palindrome {
+public static void main(String[] args) {
+
+    palindrome();
+}
+
+public static void palindrome() {
+    System.out.print("Enter the input : ");
+    Scanner input = new Scanner(System.in);
+    int num = input.nextInt();
+
+    int original = num;
+    int reversed = 0;
+
+    while (num > 0) {
+        int digit = num % 10;
+        reversed = (reversed * 10) + digit;
+        num /= 10;
+
+
+    }
+    if (original == reversed) {
+        System.out.println("The number is palindrome ");
+    } else {
+        System.out.println("it's not. ");
+    }
 }
