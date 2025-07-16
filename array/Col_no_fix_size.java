8102c40e08043712c709336b0eb27117d303047f
shubham sarkar <autu8m@shubhams-MacBook-Air.local>
2025-07-16 20:42:48 +0530

    just gonna push to mac branch

--- array/Col_no_fix_size.java
+++ array/Col_no_fix_size.java
@@ -1,2 +1,15 @@
 public class Col_no_fix_size {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, 3, 4},
+                {1, 2},
+                {1, 4, 56}
+        };
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
